e5a129100cf823b1e3825f9cdc0226559ab9d83f
ValentinaGallippi <128006557+ValentinaGallippi@users.noreply.github.com>
2024-12-07 12:06:17 -0300

    memotest 4x4

--- src/componets/GamePage.jsx
+++ src/componets/GamePage.jsx
@@ -1,15 +1,50 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Board from './Board';
 import './GamePage.css'
 
-const GamePage = () => {
-    return (
-        <div>
-            <text> jueguito </text>
-            <Link to='/finish'> 
-                <button> ponele q termino </button>
-            </Link>
-        </div>
-    )
-}
+const emojiList = [...'💣🧤🎩🌮🎱🌶🍕🦖'];
 
+const GamePage = () => {
+    const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([]);
+    const [selectedMemoBlock, setselectedMemoBlock] = useState(null);
+    const [animating, setAnimating] = useState(false);
+  
+    useEffect( () => {
+      const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
+      setShuffledMemoBlocks(shuffledEmojiList.map( (emoji, i) => ({ index: i, emoji, flipped: false}) ));
+    }, []);
+  
+    const shuffleArray = a => {
+      for (let i = a.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [a[i], a[j]] = [a[j], a[i]];
+      }
+      return a;
+    }
+  
+    const handleMemoClick = memoBlock => {
+      const flippedMemoBlock = { ...memoBlock, flipped: true };
+      let shuffledMemoBlocksCopy = [...shuffledMemoBlocks];
+      shuffledMemoBlocksCopy.splice(memoBlock.index, 1, flippedMemoBlock);
+      setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+      if(selectedMemoBlock === null) {
+        setselectedMemoBlock(memoBlock);
+      } else if(selectedMemoBlock.emoji === memoBlock.emoji) {
+        setselectedMemoBlock(null);
+      } else {
+        setAnimating(true);
+        setTimeout(() => {
+          shuffledMemoBlocksCopy.splice(memoBlock.index, 1, memoBlock);
+          shuffledMemoBlocksCopy.splice(selectedMemoBlock.index, 1, selectedMemoBlock);
+          setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+          setselectedMemoBlock(null);
+          setAnimating(false);
+        }, 1000);
+      }
+    }
+  
+    return (
+      <Board memoBlocks={shuffledMemoBlocks} animating={animating}  handleMemoClick={handleMemoClick} />
+    );
+  }
 export default GamePage;
